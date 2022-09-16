a1f52e1125363d5c495a65e97e4cb1400851e14a
Luz Maria Cerda Aldasoro <luzmariacerdaaldasoro@gmail.com>
2022-09-15 20:07:22 -0500

    Presentacion-Cameli

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Les presento a la pintora
           <p>
-            Hola profe
+            María del Carmen Aldasoro Zetina
           </p>
         </p>
         <a
